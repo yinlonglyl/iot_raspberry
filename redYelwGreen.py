6dbae08b3da81e204ac35c5e3135bf67ff161315
yinlonglyl <416009467@qq.com>
2018-02-06 16:41:01 +0800

    GPIO control example.

--- redYelwGreen.py
+++ redYelwGreen.py
@@ -5,31 +5,32 @@ import RPi.GPIO as GPIO
 import time
 
 RED = 5
-YELLOW = 6
-GREEN = 13
+#YELLOW = 6
+#GREEN = 13
 
 #Pin Setup
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-GPIO.setup(GREEN, GPIO.OUT)
-GPIO.setup(YELLOW, GPIO.OUT)
+#GPIO.setup(GREEN, GPIO.OUT)
+#GPIO.setup(YELLOW, GPIO.OUT)
 GPIO.setup(RED, GPIO.OUT)
 
 try:
     while(1):
         #print("In")
-        #time.sleep(2)
-        GPIO.output(RED, True)
-        GPIO.output(YELLOW, True)
-        GPIO.output(GREEN, True)
-        time.sleep(1)
         GPIO.output(RED, False)
-        GPIO.output(YELLOW, False)
-        GPIO.output(GREEN, False)
-        time.sleep(1)
+        time.sleep(2*60)
+        GPIO.output(RED, True)
+        #GPIO.output(YELLOW, True)
+        #GPIO.output(GREEN, True)
+        time.sleep(20*60)
+        GPIO.output(RED, False)
+        #GPIO.output(YELLOW, False)
+        #GPIO.output(GREEN, False)
+        time.sleep(5*60)
 except KeyboardInterrupt:
     print("Bye Bye")
-    GPIO.output(GREEN, False)
-    GPIO.output(YELLOW, False)
+    #GPIO.output(GREEN, False)
+    #GPIO.output(YELLOW, False)
     GPIO.output(RED, False)
 
